561b2fc883c6c4eed32862ea8b99dac16e3e53e2
Khawm Mung <johnmungtawng5@gmail.com>
2024-11-02 02:49:47 -0500

    remove experimental ppr

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
-    ppr: 'incremental', // partial prerendering for specific routes
-  },
 };
 
 export default nextConfig;
